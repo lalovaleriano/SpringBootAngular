9424c047552ae7a81defb982912c5f975987e279
lalovaleriano <eduardo.valeriano@spaymentservices.com>
2019-11-14 15:40:29 -0600

    se agregan dos cambios mas

--- app-clientes/src/app/clientes/clientes.json.ts
+++ app-clientes/src/app/clientes/clientes.json.ts
@@ -1,13 +1,13 @@
 import {Cliente}from './cliente';
 export const CLIENTES: Cliente []= [
-  {id:1, nombre:'eduardo', apellido:'valeriano', createAt:'22-22-22', email:'eduardo@gmail.com'},
-  {id:2, nombre:'eduardo', apellido:'valeriano', createAt:'22-22-22', email:'eduardo@gmail.com'},
-  {id:3, nombre:'eduardo', apellido:'valeriano', createAt:'22-22-22', email:'eduardo@gmail.com'},
-  {id:4, nombre:'eduardo', apellido:'valeriano', createAt:'22-22-22', email:'eduardo@gmail.com'},
-  {id:5, nombre:'eduardo', apellido:'valeriano', createAt:'22-22-22', email:'eduardo@gmail.com'},
-  {id:6, nombre:'eduardo', apellido:'valeriano', createAt:'22-22-22', email:'eduardo@gmail.com'},
-  {id:7, nombre:'eduardo', apellido:'valeriano', createAt:'22-22-22', email:'eduardo@gmail.com'},
-  {id:8, nombre:'eduardo', apellido:'valeriano', createAt:'22-22-22', email:'eduardo@gmail.com'},
-  {id:9, nombre:'eduardo', apellido:'valeriano', createAt:'22-22-22', email:'eduardo@gmail.com'},
-  {id:10, nombre:'eduardo', apellido:'valeriano', createAt:'22-22-22', email:'eduardo@gmail.com'}
+  {id:1, nombre:'eduardo', apellido:'valeriano', createAt:'14/11/2019', email:'eduardo@gmail.com'},
+  {id:2, nombre:'juand', apellido:'valeriano', createAt:'14/11/2019', email:'eduardo@gmail.com'},
+  {id:3, nombre:'armando', apellido:'valeriano', createAt:'14/11/2019', email:'eduardo@gmail.com'},
+  {id:4, nombre:'javier', apellido:'valeriano', createAt:'14/11/2019', email:'eduardo@gmail.com'},
+  {id:5, nombre:'omar', apellido:'valeriano', createAt:'14/11/2019', email:'eduardo@gmail.com'},
+  {id:6, nombre:'carlos', apellido:'valeriano', createAt:'14/11/2019', email:'eduardo@gmail.com'},
+  {id:7, nombre:'eduardo', apellido:'valeriano', createAt:'14/11/2019', email:'eduardo@gmail.com'},
+  {id:8, nombre:'eduardo', apellido:'valeriano', createAt:'14/11/2019', email:'eduardo@gmail.com'},
+  {id:9, nombre:'eduardo', apellido:'valeriano', createAt:'14/11/2019', email:'eduardo@gmail.com'},
+  {id:10, nombre:'eduardo', apellido:'valeriano', createAt:'14/11/2019', email:'eduardo@gmail.com'}
 ];
